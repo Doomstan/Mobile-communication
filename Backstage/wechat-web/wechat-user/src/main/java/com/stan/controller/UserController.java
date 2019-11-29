1c8b81fefb227fb275392114667fe6a2edd73ae5
Doomstan <845080286@qq.com>
2019-11-29 22:06:33 +0800

    用户模块+entity+common

--- Backstage/wechat-web/wechat-user/src/main/java/com/stan/controller/UserController.java
+++ Backstage/wechat-web/wechat-user/src/main/java/com/stan/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.stan.controller;
 
+import com.stan.entity.User;
+import com.stan.entity.base.BusinessException;
+import com.stan.entity.base.ResultEntity;
 import com.stan.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Author: Stan
@@ -21,6 +26,36 @@ public class UserController {
     private IUserService userService;
 
 
+    //测试
+    @RequestMapping("test")
+    public String test() {
+        return "test";
+    }
+
+
+    //全局异常测试
+    @RequestMapping("exception")
+    public ResultEntity exceptionTest() {
+        int i = new Random().nextInt(2);
+        if (i == 0) {
+            return ResultEntity.success();
+        } else {
+            throw new BusinessException();
+        }
+    }
+
+
+
+    //用户注册
+    @RequestMapping("registerUser")
+    public ResultEntity registerUser(User user){
+
+        //唯一性校验
+
+
+
+        return null;
+    }
 
 
 }
